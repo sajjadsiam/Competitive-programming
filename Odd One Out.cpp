3da6a179d928fe8451fe6a2a9fc3dae5f742b311
Sajjad Siam <86528048+sajjadsiam@users.noreply.github.com>
2024-02-18 03:09:02 +0600

    Create Odd One Out.cpp

---
+++ Odd One Out.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int a,b,c;
+    while(n--)
+    {
+       
+         cin>>a>>b>>c;
+         if(a==b)
+         {
+             cout<<c<<endl;
+         }
+         else if(b==c)
+         {
+             cout<< a <<endl;
+         }
+         else
+         {
+            cout<< b <<endl;
+         }
+    }
+  
+    
+   
+    return 0;
+}
