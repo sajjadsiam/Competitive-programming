9f939d891f5a15d92058b1f7a617baf7029c39c2
Sajjad Siam <86528048+sajjadsiam@users.noreply.github.com>
2024-02-18 01:48:28 +0600

    Create Elephant.cpp

---
+++ Elephant.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+ 
+using namespace std;
+ 
+int main() {
+    int x;
+    cin >> x;
+ 
+    int steps = ceil(x / 5.0);
+ 
+    cout << steps << endl;
+
+    return 0;
+}
